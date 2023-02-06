0fc8d245c900c7208ea5d4898ec8263c08d25e23
Zachary Zuber <zuber.zac@gmail.com>
2023-02-06 15:29:38 +0100

    Ajoutez des fichiers projet.

---
+++ src/mc_example.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "jlparser/parser.hpp"
+
+
+using namespace std;
+
+int main()//int argc, char **argv)
+{
+    cout << "Hellow World!" << endl;
+    
+
+    exit(0);
+}
