1ec963814ddfd8ecfed19fd08d2cf728b717699b
Julien Bensoussan <julienbensoussan@eduroam-109200.grenet.fr>
2023-02-08 10:54:07 +0100

    payoff pour call currency et call quanto

--- src/CallCurrency.cpp
+++ src/CallCurrency.cpp
@@ -1,16 +1,26 @@
 #include "Option.h"
 #include "CallCurrency.h"
+#include "pnl/pnl_vector.h"
+#include "pnl/pnl_matrix.h"
+#include <cmath>
 
-CallCurrency::CallCurrency(double T, int nbTimeSteps, int size, vector<int> assetCurrencyMapping) {
+CallCurrency::CallCurrency(double T, int nbTimeSteps, int size, vector<int> assetCurrencyMapping, int strike, double r) {
 	this->T_ = T;
 	this->size_ = size;
 	this->nbTimeSteps_ = nbTimeSteps;
     this->assetCurrencyMapping_ = assetCurrencyMapping_;
+	this->strike_ = strike;
+	this->r = r;
 }
 
 CallCurrency::~CallCurrency(){}
 
-double CallCurrency::Payoff(const PnlMat* path){
+double CallCurrency::payoff(const PnlMat* path){
     
+	double payoff = pnl_mat_get(path, this->nbTimeSteps_, pnl_vect_get(this->assetCurrencyMapping_,1))*exp(-this->r * this->T_) - this->strike;
+	if (payoff > 0){
+		return payoff;
+	}
+	return 0;
 };
 
--- src/CallQuanto.cpp
+++ src/CallQuanto.cpp
@@ -1,16 +1,23 @@
 #include "Option.h"
 #include "CallQuanto.h"
+#include "pnl/pnl_vector.h"
+#include "pnl/pnl_matrix.h"
 
-CallQuanto::CallQuanto(double T, int nbTimeSteps, int size, vector<int> assetCurrencyMapping) {
+CallQuanto::CallQuanto(double T, int nbTimeSteps, int size, vector<int> , int strike) {
 	this->T_ = T;
 	this->size_ = size;
 	this->nbTimeSteps_ = nbTimeSteps;
     this->assetCurrencyMapping_ = assetCurrencyMapping_;
+	this->strike_ = strike;
 }
 
 CallQuanto::~CallQuanto(){}
 
-double CallQuanto::Payoff(const PnlMat* path){
-    
+double CallQuanto::payoff(const PnlMat* path){
+    double payoff = pnl_mat_get(path, this->nbTimeSteps_, pnl_vect_get(this->assetCurrencyMapping_,1)) - this->strike;
+	if (payoff > 0){
+		return payoff;
+	}
+	return 0;
 };
 
